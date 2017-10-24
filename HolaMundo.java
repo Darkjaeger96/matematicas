630a494cc63219ad1a88d7897472878c695bb2aa
jesus villar <jevigar96@hotmail.com>
2017-10-24 12:32:29 +0200

    Modificado HolaMudo.Java añadiendo una nueva linea en el método main

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,12 @@
+/* Fichero HolaMundo usado para el tema de Git
+ * @Author jesus villar
+ * @Version 1.0*/
 
 public class HolaMundo {
 	
 	public static void main (String args[]) {
 		System.out.println("Hola mundo");
+		System.out.println("Hello World");
 		
 	}
 }
